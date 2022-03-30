e75a09cf218e0197a220f2fbd134b8f776eb546c
Camilo-09 <jcmilogomez30@gmail.com>
2022-03-30 13:07:58 -0500

    correpcion de error

--- Views/pages/firma/conexion.php
+++ Views/pages/firma/conexion.php
@@ -1,21 +1,17 @@
 <?php
 
 $serverName = "190.242.112.94";
-$connectionOptions = array("Database"=>"sena",
-<<<<<<< HEAD
-            "Uid"=>"sa", "PWD"=>"Admin@*", "CharacterSet"=>"UTF-8");
-=======
-            "Uid"=>"Ota", "PWD"=>"Sena2022", "CharacterSet"=>"UTF-8");
->>>>>>> 8b80916b7d3201067fc51c9af62b4d58bd09e96e
+$connectionOptions = array("Database"=>"sena","Uid"=>"Ota", "PWD"=>"Sena2022", "CharacterSet"=>"UTF-8");
+
 $conn = sqlsrv_connect($serverName, $connectionOptions);
-if($conn ){
+/*if($conn ){
     echo "Conexion establecida";
 }
 else{
     echo "La conexion no se pudo realizar";
     die (print_r(sqlsrv_errors(),true));
      
-}
+}*/
 
 
 return $conn; 
