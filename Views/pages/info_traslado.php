8146ebb0bc0d9de4fbc09093152a988c96c95c95
Joiner2003 <joineredh@gmail.com>
2022-04-23 08:52:26 -0500

    Pdf con DOMPDF

--- Views/pages/info_traslado.php
+++ Views/pages/info_traslado.php
@@ -1,21 +1,42 @@
 <?php
+ ob_start();
 
-require_once('../../Controllers/HomeController.php');
-$HomeController = new HomeController();
-date_default_timezone_set('America/Bogota');
-session_start();
-$ips = $HomeController->VerIPS();
-if (isset($_GET['IdServicio'])) {
-    $IdServicio = $_GET['IdServicio'];
-    $data = $HomeController->VerOta_Informe_Traslado($IdServicio);
-  }else{
-    $IdServicio = $HomeController->MaximoOta_Informe_Traslado()->__GET('IdServicio')+1;
-    $data = NULL;
-  }
-  require_once '../../Lib/dompdf/autoload.inc.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
+<?php
+
+$html= ob_get_clean();
+//echo $html;
+
+require_once '../../Lib/dompdf/autoload.inc.php';
 use Dompdf\Dompdf;
 $dompdf=new Dompdf();
-$pdf->SetHeader($html);
+
+$options = $dompdf->getOptions();
+$options->set(array('isRemoteEnabled' => true));
+$dompdf->setOptions($options);
+
+$dompdf->loadHtml($html);
+$dompdf->setPaper('letter');
+
+$dompdf->render();
+$dompdf->stream("traslado_.pdf", array("Attachment"=>false))
+/*$pdf->SetHeader($html);
 $pdf->WriteHTML($content); 
-$pdf->Output();
+$pdf->Output();*/
 ?>
