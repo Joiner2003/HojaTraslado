4a82b513d3f951c4c99e4978e40f24ca4ceffa60
Joiner2003 <joineredh@gmail.com>
2022-04-23 10:51:47 -0500

    PDF Con Imagenes y valores

--- Views/pages/info_traslado.php
+++ Views/pages/info_traslado.php
@@ -2,7 +2,29 @@
  ob_start();
 
 ?>
+<?php
 
+require_once '../../Lib/dompdf/autoload.inc.php';
+use Dompdf\Dompdf;
+$dompdf=new Dompdf();
+
+require_once('../../Controllers/HomeController.php');
+$HomeController = new HomeController();
+date_default_timezone_set('America/Bogota');
+session_start();
+
+$ips = $HomeController->VerIPS();
+if (isset($_GET['IdServicio'])) {
+    $IdServicio = $_GET['IdServicio'];
+    $data = $HomeController->VerOta_Informe_Traslado($IdServicio);
+  }else{
+    $IdServicio = $HomeController->MaximoOta_Informe_Traslado()->__GET('IdServicio')+1;
+    $data = NULL;
+  }
+
+  $nombreImagen = "../Resource/img/logoota2.jpg";
+  $imagenBase64 = "data:image/png;base64," . base64_encode(file_get_contents($nombreImagen));
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,17 +34,19 @@
   <title>Document</title>
 </head>
 <body>
+  
 <table width="100%" border="1" style="font-size: 12px">
     <tr>
         <td rowspan="4" colspan="2" align="center">
-            <img src="../Resource/img/logoota2.jpg" width="120" height="120">
+            <img src="<?php echo $imagenBase64?>" width="120" height="120">
         </td>
         <td rowspan="4" colspan="4" align="center" valign="middle" width="60%">
             <strong style="font-size: 16px">HOJA DE TRASLADOS</strong><br>
-            '.$ips->__GET('NombreIPS').' <br>
-            NIT. '.$ips->__GET('Nit').'<br>
-            '.$ips->__GET('Direccion').' '.$ips->__GET('Municipio').' '.$ips->__GET('Departamento').'<br>
-            Telefono: '.$ips->__GET('Telefono').'
+           <?php echo $ips->__GET('NombreIPS') ?><br>
+           <?php echo "NIT ".$ips->__GET('Nit')?><br>
+           <?php echo $ips->__GET('Direccion')?><br>
+           <?php echo $ips->__GET('Municipio').' '.$ips->__GET('Departamento')?><br>
+           <?php echo "Telefono:".$ips->__GET('Telefono')?>
         </td>
         <td colspan="1"><strong>Codigo:</strong></td>
         <td colspan="1" align="center"></td>
@@ -33,49 +57,48 @@
     </tr>
     <tr>
         <td colspan="1"><strong>Fecha:</strong></td>
-        <td colspan="1" align="center">'.date('d/m/Y').'</td>
+        <td colspan="1" align="center"><?php echo date('d/m/Y')?></td>
     </tr>
     <tr>
         <td colspan="1"><strong>Pagina:</strong></td>
-        <td colspan="1" align="center">'."{PAGENO}"." De "."{nb}".'</td>
+        <td colspan="1" align="center"><?php echo  "PAGE_NUM of PAGE_COUNT"?></td>
     </tr>
 </table><br>
-';
-$content ='
+
 <table style="width:100%">
   <tr>
     <th colspan="4">Informacion General Del Paciente</th>
   </tr>
   <tr>
-    <td colspan="2">Nombres: '.$data->__GET('PteNom1').' '.$data->__GET('Pte_Nom2').'</td>
-    <td colspan="2">Apellidos: '.$data->__GET('Pte_Ap1').' '.$data->__GET('Pte_Ap2').' </td>
+    <td colspan="2"><?php echo "Nombres: ". $data->__GET('PteNom1').' '.$data->__GET('Pte_Nom2')?></td>
+    <td colspan="2"><?php echo "Apellidos: " .$data->__GET('Pte_Ap1').' '.$data->__GET('Pte_Ap2')?> </td>
   </tr>
   <tr>
-    <td>Identificación:  '.$data->__GET('Pte_NumDoc').'</td>
-    <td>T Identificación:  '.$data->__GET('Pte_TipoDoc').' </td>
-    <td>Edad:  '.$data->__GET('Pte_Edad').'</td>
+    <td> <?php echo "Identificación: " .$data->__GET('Pte_NumDoc')?></td>
+    <td> <?php echo "T Identificación: " .$data->__GET('Pte_TipoDoc') ?></td>
+    <td> <?php echo "Edad: " .$data->__GET('Pte_Edad') ?></td>
     </tr>
     <tr>
-    <td>Fecha Nacimiento: '.$data->__GET('Pte_FechaNac').'</td>
-    <td>Entidad: '.$data->__GET('').'</td>
-    <td>Regimen: '.$data->__GET('Pte_Regimen').'</td>
+    <td><?php echo "Fecha Nacimiento: " .$data->__GET('Pte_FechaNac')?></td>
+    <td>Entidad:</td>
+    <td><?php echo "Regimen: ".$data->__GET('Pte_Regimen')?></td>
   </tr>
    <tr>
-  <td>Telefono: '.$data->__GET('Pte_Telefono').' </td>
+  <td> <?php echo "Telefono: ".$data->__GET('Pte_Telefono') ?> </td>
   <td></td>
-  <td>Direccion: '.$data->__GET('Pte_Direccion').'</td>
+  <td><?php echo "Direccion: ".$data->__GET('Pte_Direccion') ?></td>
   </tr>
   <tr>
     <th colspan="4">Informacion Del Acompañante</th>
   </tr>
    <tr>
-    <td colspan="2">Nombres: '.$data->__GET('Aco_Nombres').'</td>
-    <td colspan="2">Apellidos: '.$data->__GET('Aco_Apellidos').'</td>
+    <td colspan="2"><?php echo 'Nombres: '.$data->__GET('Aco_Nombres') ?></td>
+    <td colspan="2"><?php echo 'Apellidos: '.$data->__GET('Aco_Apellidos') ?></td>
   </tr>
   <tr>
-    <td>Identificación: '.$data->__GET('Aco_Documento').'</td>
+    <td><?php echo 'Identificación: '.$data->__GET('Aco_Documento') ?></td>
     <td></td>
-    <td>Parentesco: '.$data->__GET('Aco_Perentezco').'</td>
+    <td> <?php echo 'Parentesco: '.$data->__GET('Aco_Perentezco')?></td>
   </tr>
   </table>
   <br>
@@ -86,29 +109,29 @@ $content ='
    <tr>
    </tr>
   <tr>
-    <td>Origen 1:  '.$data->__GET('Sv_Origen').'</td>
-     <td>Llegada:  '.$data->__GET('Sv_Llegada').'</td>
+    <td><?php echo 'Origen 1:  '.$data->__GET('Sv_Origen')?></td>
+     <td><?php echo 'Llegada:  '.$data->__GET('Sv_Llegada')?></td>
     <td></td>
-    <td>Salida:  '.$data->__GET('Sv_Salida').'</td>
+    <td><?php echo 'Salida:  '.$data->__GET('Sv_Salida')?></td>
     </tr>
     <tr>
-    <td>Destino 1:  '.$data->__GET('Sv_Origen1').'</td>
-    <td>Llegada:  '.$data->__GET('Sv_Llegada1').'</td>
+    <td><?php echo 'Destino 1:  '.$data->__GET('Sv_Origen1')?></td>
+    <td><?php echo 'Llegada:  '.$data->__GET('Sv_Llegada1')?></td>
     <td></td>
-    <td>Salida:  '.$data->__GET('Sv_Salida1').'</td>
+    <td><?php echo 'Salida:  '.$data->__GET('Sv_Salida1')?></td>
   </tr>
   <tr>
-    <td>Destino 2:  '.$data->__GET('Sv_Origen2').'</td>
-    <td>Llegada:  '.$data->__GET('Sv_Llegada2').'</td>
+    <td><?php echo 'Destino 2:  '.$data->__GET('Sv_Origen2')?></td>
+    <td><?php echo 'Llegada:  '.$data->__GET('Sv_Llegada2')?></td>
     <td></td>
-    <td>Salida:  '.$data->__GET('Sv_Salida2').'</td>
+    <td><?php echo 'Salida:  '.$data->__GET('Sv_Salida2')?></td>
   </tr>
   <tr>
-   <td>Complejidad Del Servicio:  '.$data->__GET('Sv_Complejidad').'</td>
-   <td>Tipo De Servicio:  '.$data->__GET('Sv_TipoServicio').'</td>
+   <td><?php echo 'Complejidad Del Servicio:  '.$data->__GET('Sv_Complejidad')?></td>
+   <td><?php echo 'Tipo De Servicio:  '.$data->__GET('Sv_TipoServicio')?></td>
   </tr>
   <tr>
-  <td>Examen Solicitado: '.$data->__GET('Sv_ExamenSolicitado').'</td>
+  <td><?php echo 'Examen Solicitado: '.$data->__GET('Sv_ExamenSolicitado')?></td>
  </tr>
 </table>
 <br>
@@ -117,22 +140,22 @@ $content ='
    <th colspan="4">Examen Físico Pre-Traslado</th>
  </tr>
  <tr>
-   <td>T/A: '.$data->__GET('Ef_Ta').'</td>
-   <td>F.R: '.$data->__GET('Ef_Fr').'</td>
+   <td><?php echo 'T/A: '.$data->__GET('Ef_Ta')?></td>
+   <td><?php echo 'F.R: '.$data->__GET('Ef_Fr')?></td>
    </tr>
    <tr>
-   <td>TEMP: '.$data->__GET('Ef_Temp').'</td>
-   <td>GLASSGOW: '.$data->__GET('Ef_Glasgow').'</td>
+   <td><?php echo 'TEMP: '.$data->__GET('Ef_Temp')?></td>
+   <td><?php echo 'GLASSGOW: '.$data->__GET('Ef_Glasgow')?></td>
  </tr>
  <tr>
-   <td>Diagnostico 1: '.$data->__GET('Ef_Dx1').'</td>
-   <td>Diagnostico 2: '.$data->__GET('Ef_Dx2').'</td>
+   <td><?php echo 'Diagnostico 1: '.$data->__GET('Ef_Dx1')?></td>
+   <td><?php echo 'Diagnostico 2: '.$data->__GET('Ef_Dx2')?></td>
  </tr>
  <tr>
-  <td>Hallazgos Positivos: '.$data->__GET('Ef_HallazgoPos1').'</td>
+  <td><?php echo 'Hallazgos Positivos: '.$data->__GET('Ef_HallazgoPos1')?></td>
  </tr>
  <tr>
-  <td>Antecedentes: '.$data->__GET('Ef_Antecedentes1').'</td>
+  <td><?php echo 'Antecedentes: '.$data->__GET('Ef_Antecedentes1')?></td>
  </tr>
  </table>
  <table style="width:100% ">
@@ -140,11 +163,11 @@ $content ='
    <th colspan="4">Ginecológicos</th>
  </tr>
  <tr>
-   <td>G: '.$data->__GET('Ef_Gin1').'</td>
-   <td>P: '.$data->__GET('Ef_Gin2').'</td>
-   <td>C: '.$data->__GET('Ef_Gin3').'</td>
-   <td>A: '.$data->__GET('Ef_Gin4').'</td>
-   <td>V: '.$data->__GET('Ef_Gin5').'</td>
+   <td><?php echo 'G: '.$data->__GET('Ef_Gin1')?></td>
+   <td><?php echo 'P: '.$data->__GET('Ef_Gin2')?></td>
+   <td><?php echo 'C: '.$data->__GET('Ef_Gin3')?></td>
+   <td><?php echo 'A: '.$data->__GET('Ef_Gin4')?></td>
+   <td><?php echo 'V: '.$data->__GET('Ef_Gin5')?></td>
    </tr>
    </table>
  <table style="width:100% ">
@@ -152,19 +175,19 @@ $content ='
    <th colspan="4">Ventilacion Mecánica</th>
  </tr>
    <tr>
-  <td>Oxígeno: '.$data->__GET('Ef_Modo1').'</td>
-   <td>VOL:</td>
-   <td>P.E.E.P.: '.$data->__GET('Ef_Modo2').'</td>
+  <td><?php echo 'Oxígeno: '.$data->__GET('Ef_Modo1')?></td>
+   <td><?php echo 'VOL:'?></td>
+   <td><?php echo 'P.E.E.P.: '.$data->__GET('Ef_Modo2')?></td>
    </tr>
    <tr>
-   <td>Inotropia: '.$data->__GET('Ef_Modo3').'</td>
-   <td>FR: '.$data->__GET('Ef_Modo4').'</td>
-   <td>FIOZ: '.$data->__GET('Ef_Modo5').'</td>
+   <td><?php echo 'Inotropia: '.$data->__GET('Ef_Modo3')?></td>
+   <td><?php echo 'FR: '.$data->__GET('Ef_Modo4')?></td>
+   <td><?php echo 'FIOZ: '.$data->__GET('Ef_Modo5')?></td>
    </tr>
    <tr>
-  <td>Firma Paciente o Acompañante:  <img src='. $data->__GET('Firma1').'></td>
+  <td><?php echo 'Firma Paciente o Acompañante:  <img src='. $data->__GET('Firma1')?>></td>
   <td></td>
-  <td>Firma Médico o Enfermera(o) Que Entrega:  <img src='. $data->__GET('Firma2').'></td>
+  <td><?php echo 'Firma Médico o Enfermera(o) Que Entrega:  <img src='. $data->__GET('Firma2')?>></td>
    </tr>
    </table>
    <table style="width:100% ">
@@ -172,17 +195,18 @@ $content ='
       <th colspan="4">Signos Vitales</th>
     </tr>
     <tr>
-      <td>Fecha: '.$data->__GET('Sv_ExamenSolicitado').'</td>
-      <td>Hora: '.$data->__GET('Sv_ExamenSolicitado').'</td>
-      <td>T/A: '.$data->__GET('Sv_ExamenSolicitado').'</td>
-      <td>F.C: '.$data->__GET('Sv_ExamenSolicitado').'</td>
-      </tr>
-      <tr>
-      <td>F.R: '.$data->__GET('Sv_ExamenSolicitado').'</td>
-      <td>Temp: '.$data->__GET('Sv_ExamenSolicitado').'</td>
-      <td>Glassgow: '.$data->__GET('Sv_ExamenSolicitado').'</td>
-      <td>SatO2: '.$data->__GET('Sv_ExamenSolicitado').'</td>
-
+    <?php foreach ($HomeController->ListarxIdServicioOta_Informe_SignosVitales($IdServicio) as $key): ?>
+                        <tr class="text-center">
+                          <td><?= print("Fecha: ").(new DateTime($key->__GET('FechaHora')))->format('d/m/Y') ?></td>
+                          <td><?= print("Hora: ").(new DateTime($key->__GET('FechaHora')))->format('H:i') ?></td>
+                          <td><?= print("TA: ").$key->__GET('TA') ?></td>
+                          <td><?= print("FC: ").$key->__GET('FC') ?></td>
+                          <td><?= print("FR: ").$key->__GET('FR') ?></td>
+                          <td><?= print("TEMP: ").$key->__GET('Temp') ?></td>
+                          <td><?= print("GLASGOW: ").$key->__GET('Glasgow') ?></td>
+                          <td><?= print("SAT02: ").$key->__GET('SatO2') ?></td>
+                         </tr>
+                      <?php endforeach ?>
     </tr>
    </table>
    <table style="width:100% ">
@@ -190,14 +214,16 @@ $content ='
       <th colspan="4">Control De Liquidos Y Medicamentos</th>
     </tr>
     <tr>
-      <td>L.E.V:'.$data->__GET('Sv_ExamenSolicitado').'</td>
-      <td>Goteo: '.$data->__GET('Sv_ExamenSolicitado').'</td>
-      <td>Cantidad Infundida: '.$data->__GET('Sv_ExamenSolicitado').'</td>
-      </tr>
-      <tr>
-      <td>Inotrópico: '.$data->__GET('Sv_ExamenSolicitado').'</td>
-      <td>Goteo: '.$data->__GET('Sv_ExamenSolicitado').'</td>
-      <td>Cantidad Infundida: '.$data->__GET('Sv_ExamenSolicitado').'</td>
+    <?php foreach ($HomeController->ListarxIdServicioOta_Informe_Liquidos($IdServicio) as $key): ?>
+                        <tr class="text-center">
+                          <td><?=  print("LEV: ").$key->__GET('LEV') ?></td>
+                          <td><?=  print("Goteo: ").$key->__GET('Goteo') ?></td>
+                          <td><?=  print("Cantidad: ").$key->__GET('Cantidad') ?></td>
+                          <td><?=  print("Inotropico: ").$key->__GET('Inotropico') ?></td>
+                          <td><?=  print("Goteo2: ").$key->__GET('Goteo2') ?></td>
+                          <td><?=  print("Cantidad2: ").$key->__GET('Cantidad2') ?></td>
+                          </tr>
+                      <?php endforeach ?> 
     </tr>
    </table>
    <table style="width:100% ">
@@ -205,11 +231,12 @@ $content ='
       <th colspan="4">Estado Del Paciente Al Finalizar Traslado</th>
     </tr>
      <tr>
-      <td>Estado Del Paciente Al Finalizar Traslado: '.$data->__GET('Estado_ft').'</td>
+      <td><?php echo 'Estado Del Paciente Al Finalizar Traslado: '.$data->__GET('Estado_ft')?></td>
     </tr>
     <tr>
-      <td>Observaciones Al Momento De La Entrega: '.$data->__GET('Obs_entrega').'</td>
+      <td><?php echo 'Observaciones Al Momento De La Entrega: '.$data->__GET('Obs_entrega')?></td>
     </tr>
+
    
 </table>
 </body>
@@ -222,16 +249,13 @@ $content ='
 $html= ob_get_clean();
 //echo $html;
 
-require_once '../../Lib/dompdf/autoload.inc.php';
-use Dompdf\Dompdf;
-$dompdf=new Dompdf();
 
 $options = $dompdf->getOptions();
 $options->set(array('isRemoteEnabled' => true));
 $dompdf->setOptions($options);
 
 $dompdf->loadHtml($html);
-$dompdf->setPaper('letter');
+$dompdf->setPaper('A4', 'portrait');
 
 $dompdf->render();
 $dompdf->stream("traslado_.pdf", array("Attachment"=>false))
