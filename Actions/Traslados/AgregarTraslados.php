f3c2916b4d2c1063dd6bda2696e9cd9ab32a21c7
Camilo-09 <jcmilogomez30@gmail.com>
2022-04-27 17:06:46 -0500

    Correcion de guardado

--- Actions/Traslados/AgregarTraslados.php
+++ Actions/Traslados/AgregarTraslados.php
@@ -56,59 +56,11 @@ if(!isset($_SESSION["Usuario"])){
 	if(isset($_POST['Sv_Complejidad'])){$Sv_Complejidad = $_POST['Sv_Complejidad'];}else{$Sv_Complejidad = 0;}
 	if(isset($_POST['Sv_TipoServicio'])){$Sv_TipoServicio = $_POST['Sv_TipoServicio'];}else{$Sv_TipoServicio = 0;}
 	$Sv_ExamenSolicitado = $_POST['Sv_ExamenSolicitado'];
-	$Ef_Ta = $_POST['Ef_Ta'];
-	$Ef_Fr = $_POST['Ef_Fr'];
-	$Ef_Temp = $_POST['Ef_Temp'];
-	$Ef_Glasgow = $_POST['Ef_Glasgow'];
-	$Ef_Dx1 = $_POST['Ef_Dx1'];
-	$Ef_Dx2 = $_POST['Ef_Dx2'];
-	$Ef_HallazgoPos1 = $_POST['Ef_HallazgoPos1'];
-	$Ef_Antecedentes1 = $_POST['Ef_Antecedentes1'];
-	$Ef_Gin1 = $_POST['Ef_Gin1'];
-	$Ef_Gin2 = $_POST['Ef_Gin2'];
-	$Ef_Gin3 = $_POST['Ef_Gin3'];
-	$Ef_Gin4 = $_POST['Ef_Gin4'];
-	$Ef_Gin5 = $_POST['Ef_Gin5'];
 
-	/*if ($_FILES["Sv_Firma_Pte"]["name"]) {
-		$NombreArchivo = $_FILES["Sv_Firma_Pte"]["name"];
-		$Sv_Firma_Pte2tmp=$_FILES['Sv_Firma_Pte']['tmp_name'];
-
-		move_uploaded_file($Sv_Firma_Pte2tmp,"Sv_Firma_Pte.png");
-		$im1 = file_get_contents("Sv_Firma_Pte.png");
-		$Sv_Firma_Pte2 = base64_encode($im1);
-
-	}else{
-		$Sv_Firma_Pte2 = '';
-	}
-
-	if ($_FILES["En_Firma"]["name"]) {
-		$NombreArchivo = $_FILES["En_Firma"]["name"];
-		$En_Firma2tmp=$_FILES['En_Firma']['tmp_name'];
-
-		move_uploaded_file($En_Firma2tmp,"En_Firma.png");
-		$im1 = file_get_contents("En_Firma.png");
-		$En_Firma2 = base64_encode($im1);
-
-	}else{
-		$En_Firma2 = '';
-	}
-
-	if ($_FILES["Sv_Firma_Entrega"]["name"]) {
-		$NombreArchivo = $_FILES["Sv_Firma_Entrega"]["name"];
-		$Sv_Firma_Entrega2tmp=$_FILES['Sv_Firma_Entrega']['tmp_name'];
-
-		move_uploaded_file($Sv_Firma_Entrega2tmp,"Sv_Firma_Entrega.png");
-		$im1 = file_get_contents("Sv_Firma_Entrega.png");
-		$Sv_Firma_Entrega2 = base64_encode($im1);
-
-	}else{
-		$Sv_Firma_Entrega2 = '';
-	}*/
 
 	// var_dump($Fecha1, $Ficha, $Pte_NumDoc, $Pte_TipoDoc, $Pte_FechaNac, $Pte_Edad, $Pte_Ap1, $Pte_Ap2, $PteNom1, $Pte_Nom2, $Aco_Nombres, $Aco_Apellidos, $Aco_Documento, $Aco_Perentezco, $Sv_Origen, $Sv_Origen1, $Sv_Origen2, $Sv_Origen3, $Sv_Llegada, $Sv_Llegada1, $Sv_Llegada2, $Sv_Llegada3, $Sv_Salida, $Sv_Salida1, $Sv_Salida2, $Sv_Salida3, $Sv_Complejidad, $Sv_TipoServicio, $Sv_ExamenSolicitado, $Ef_Ta, $Ef_Fr, $Ef_Temp, $Ef_Glasgow, $Ef_Dx1, $Ef_Dx2, $Ef_HallazgoPos1, $Ef_Antecedentes1, $Ef_Gin1, $Ef_Gin2, $Ef_Gin3, $Ef_Gin4, $Ef_Gin5);
 
-	if($HomeController->AgregarOta_Informe_Traslado($Fecha1, $Ficha, $Pte_NumDoc, $Pte_TipoDoc, $Pte_FechaNac, $Pte_Edad, $Entidad,$Pte_Regimen,$Pte_Sexo, $Pte_Telefono,$Pte_Direccion,$Pte_Ap1, $Pte_Ap2, $PteNom1, $Pte_Nom2, $Aco_Nombres, $Aco_Apellidos, $Aco_Documento, $Aco_Perentezco, $Sv_Origen, $Sv_Origen1, $Sv_Origen2, $Sv_Origen3, $Sv_Llegada, $Sv_Llegada1, $Sv_Llegada2, $Sv_Llegada3, $Sv_Salida, $Sv_Salida1, $Sv_Salida2, $Sv_Salida3, $Sv_Complejidad, $Sv_TipoServicio, $Sv_ExamenSolicitado,$Ef_Ta, $Ef_Fr, $Ef_Temp, $Ef_Glasgow, $Ef_Dx1, $Ef_Dx2, $Ef_HallazgoPos1, $Ef_Antecedentes1, $Ef_Gin1, $Ef_Gin2, $Ef_Gin3, $Ef_Gin4, $Ef_Gin5) == true){
+	if($HomeController->AgregarOta_Informe_Traslado($Fecha1, $Ficha, $Pte_NumDoc, $Pte_TipoDoc, $Pte_FechaNac, $Pte_Edad, $Entidad,$Pte_Regimen,$Pte_Sexo, $Pte_Telefono,$Pte_Direccion,$Pte_Ap1, $Pte_Ap2, $PteNom1, $Pte_Nom2, $Aco_Nombres, $Aco_Apellidos, $Aco_Documento, $Aco_Perentezco, $Sv_Origen, $Sv_Origen1, $Sv_Origen2, $Sv_Origen3, $Sv_Llegada, $Sv_Llegada1, $Sv_Llegada2, $Sv_Llegada3, $Sv_Salida, $Sv_Salida1, $Sv_Salida2, $Sv_Salida3, $Sv_Complejidad, $Sv_TipoServicio, $Sv_ExamenSolicitado) == true){
 
 		$msg->success('!Agregado con exito¡');
 	/*	unlink("Sv_Firma_Pte.png");
