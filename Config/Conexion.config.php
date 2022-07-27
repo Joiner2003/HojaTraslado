67ded34a9e0b83d70a8b70a97a5c78db1b346b62
Camilo-09 <jcmilogomez30@gmail.com>
2022-07-27 17:27:06 -0500

    Cambio a mysql

--- Config/Conexion.config.php
+++ Config/Conexion.config.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 
 define('DB_HOST', '192.168.2.199');
 define('DB_DATABASE', 'Sena');
@@ -7,12 +7,18 @@ define('DB_PORT', '');
 define('DB_USERNAME', 'sa');
 define('DB_PASSWORD', 'Dayan1001*');
 
+*/
+/*
+ define('HOST', 'otaips.com');
+ define('DBNAME', 'otaips_otatraslado');
+ define('PORT', '3306');
+ define('USERNAME', 'otaips_otatraslado');
+ define('PASSWORD', 'Otatraslado2022');
+*/
 
-
-// define('HOST', '127.0.0.1');
-// define('DBNAME', 'hojatraslado');
-// define('PORT', '');
-// define('USERNAME', 'root');
-// define('PASSWORD', '');
-
-?>
+define('HOST', 'localhost');
+define('DBNAME', 'sena');
+define('PORT', '3307');
+define('USERNAME', 'root');
+define('PASSWORD', 'Otatraslado2022');
+?> 
--- Models/Conexion/Conexion.php
+++ Models/Conexion/Conexion.php
@@ -9,10 +9,10 @@
 		public static function Conectar()
 		{
 			// configurar la coneccion
-			$dsn = 'sqlsrv:Server=' .DB_HOST. ';Database=' .DB_DATABASE;
+			//$dsn = 'sqlsrv:Server=' .DB_HOST. ';Database=' .DB_DATABASE;
 			//$dsn = new PDO("sqlsrv:Server=localhost;Database=testdb", "NombreUsuario", "Contraseña");
 
-			try
+			/*try
 			{	
 				$pdo = new PDO($dsn, DB_USERNAME, DB_PASSWORD);
 			//$pdo = new PDO('sqlsrv:Server=190.242.112.94; Database=sena', 'Ota', 'Sena2022*');
@@ -22,23 +22,23 @@
 			}catch(PDOException $e)
 			{
 				die($e->getMessage()." -> No hay Conexion a la Base de Datos");
-			}
+			}*/
 			// 
 			/// configurar la coneccion
-			// $dsn = 'mysql:host='.HOST.';dbname='.DBNAME.';charset=UTF8';
-			// $mOptions=array(
-			// 	PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
-			// 	PDO::ATTR_EMULATE_PREPARES => FALSE,
-			// );
-			// try
-			// {	
-			// 	$pdo = new PDO($dsn, USERNAME, PASSWORD, $mOptions);
-			// 	return $pdo;
-			// }catch(Exception $e)
-			// {
-			// 	die($e->getMessage()." -> No hay Conexion a la Base de Datos");
-			// }
+			$dsn = 'mysql:host='.HOST.';port='.PORT.';dbname='.DBNAME.';charset=UTF8';
+			$mOptions=array(
+				PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
+				PDO::ATTR_EMULATE_PREPARES => FALSE,
+			);
+			try
+			{	
+				$pdo = new PDO($dsn, USERNAME, PASSWORD, $mOptions);
+				return $pdo;
+			}catch(Exception $e)
+			{
+				die($e->getMessage()." -> No hay Conexion a la Base de Datos");
+			}
 		}
-		// ventas@hostingmontevideo.com
+		
 	}
 	?>
